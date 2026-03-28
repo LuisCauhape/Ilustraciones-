7fe54c2b3d104e44415fdcafb8c7c86c3de1387d
LuisCauhape <117215383+LuisCauhape@users.noreply.github.com>
2026-03-28 19:57:48 -0300

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -20,7 +20,7 @@ const projects = [
     description: "Exploración de pinceles personalizados para lograr un estilo de pintura digital único. Cada pieza busca capturar la luz y la atmósfera de escenas naturales.",
     tools: ["Photoshop", "Pinceles custom", "Wacom"],
     images: [
-      "./ilustraciones/Mesa de trabajo 1.jpg",
+      "../ilustraciones/Mesa de trabajo 1.jpg",
       "./ilustraciones/Flor1.jpg",
       "./ilustraciones/Flor2.jpg",
       "./ilustraciones/Flor3.jpg",
