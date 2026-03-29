df46ebb56cba1f6c78012183aafb4eaa53b9e3a7
LuisCauhape <117215383+LuisCauhape@users.noreply.github.com>
2026-03-28 21:16:05 -0300

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -10,7 +10,7 @@ const projects = [
     description: "Momento exacto en que la infancia, el hogar y los sueños dejan de ser seguros. Una composición oscura que captura la tensión entre lo familiar y lo ominoso.",
     tools: ["Photoshop", "Wacom", "ArtStation"],
     images: [
-  "Ilustraciones/luis-diseno-nightmare-tres-artstation.webp",
+  "luis-diseno-nightmare-tres-artstation.webp",
 ]
   },
   {
